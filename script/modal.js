6a264222ab312836e7f0527749bddc6770f68a44
Oleksiy Kyryk <kirikoleksiy112@gmail.com>
2025-05-02 21:56:38 +0300

    edit functionality buttons, add modal.js file

---
+++ script/modal.js
@@ -0,0 +1,35 @@
+(() => {
+  const refs = {
+    // Додати атрибут data-modal-open на кнопку відкриття
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    // Додати атрибут data-modal-close на кнопку закриття
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    // Додати атрибут data-modal на бекдроп модалки
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
+    refs.modal.classList.toggle("is-open");
+  }
+
+  const refs2 = {
+    // Додати атрибут data-modal-menu-open на кнопку відкриття
+    openModalBtn: document.querySelector("[data-modal-menu-open]"),
+    // Додати атрибут data-modal-menu-close на кнопку закриття
+    closeModalBtn: document.querySelector("[data-modal-menu-close]"),
+    // Додати атрибут data-modal-menu на бекдроп модалки
+    modal: document.querySelector("[data-modal-menu]"),
+  };
+
+  refs2.openModalBtn.addEventListener("click", toggleModal2);
+  refs2.closeModalBtn.addEventListener("click", toggleModal2);
+
+  function toggleModal2() {
+    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
+    refs2.modal.classList.toggle("is-open");
+  }
+})();
